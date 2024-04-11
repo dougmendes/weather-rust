f4ba8246d8899fd22224b5de29efa1c55dd35370
Douglas Mendes <douglasleomendes@hotmail.com>
2024-04-11 10:51:46 +0100

    chore: refactor code to write the tests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use ipgeolocate::{Locator, Service};
+use ipgeolocate::{Locator, Service, GeoError};
 use tokio;
 use reqwest;
-
+use async_trait::async_trait;
 
 
 #[tokio::main]
@@ -10,19 +10,32 @@ async fn main() -> Result<(), reqwest::Error>{
         .await?
         .text()
         .await?;
-    get_location(&ip).await;
+
+    let service = RealLocationService;
+
+    get_location(&service,&ip).await;
 
     Ok(())
 }
 
-async fn get_location(ip: &str) -> (){
-    let service = Service::IpApi;
-
-    match Locator::get(ip, service).await{
-        Ok(response) => {
-            println!("{:?}", response.city);
-        },
+async fn get_location<S: LocationService>(service: &S, ip: &str){
+    match service.get_location(ip).await{
+        Ok(response) => println!("{:?}", response.city),
         Err(error) => println!("Error: {}", error),
+    }  
+}
+
+#[async_trait]
+pub trait LocationService {
+    async fn get_location(&self, ip: &str) -> Result<Locator, GeoError>;
+}
+
+struct RealLocationService;
+
+#[async_trait]
+impl LocationService for RealLocationService {
+    async fn get_location(&self, ip: &str) -> Result<Locator, GeoError>{
+        Locator::get(ip, Service::IpApi).await
     }
     
 }
